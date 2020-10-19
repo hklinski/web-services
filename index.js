3b5d2aa2bcdf62c15741482d312a52b934088ab7
hklinski <hklinski@edu.cdv.pl>
2020-10-19 16:26:53 +0200

    zad1

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const helmet = require("helmet")
+var bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/soap', (req, res) => {
+    res.send('mydlo')
+  })
+
+app.get('/rest', (req, res) => {
+    res.send('reszta')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
